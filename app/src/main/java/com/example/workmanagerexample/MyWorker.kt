41158d07c7447fb728df5b108c29e79b1709ec95
F Tcymbal <ftsymbal@gmail.com>
2024-04-08 00:07:29 +0200

    Memory leak

--- app/src/main/java/com/example/workmanagerexample/MyWorker.kt
+++ app/src/main/java/com/example/workmanagerexample/MyWorker.kt
@@ -1,6 +1,7 @@
 package com.example.workmanagerexample
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
@@ -10,11 +11,16 @@ import kotlinx.coroutines.withContext
 
 class MyWorker(appContext: Context, workerParams: WorkerParameters):
     CoroutineWorker(appContext, workerParams) {
+
     override suspend fun doWork(): Result {
         withContext(Dispatchers.Main) {
             val toast = Toast.makeText(applicationContext, "Started Work", Toast.LENGTH_SHORT)
             toast.show()
         }
+
+        Log.d("GGG", "doWork: ")
+        //Emulate data required for the task.
+        val data = ByteArray(10 * 1024 * 1024) // Allocate 10 MB of memory
 
         // Emulate 10 second work
         delay(10000)
